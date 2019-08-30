ab94fcf85fb298b61c6750ee4673351928f3a94f
vfernandez84 <victor.fernandez@sage.com>
2019-08-30 14:39:40 +0200

    First commit

---
+++ ChainOfResponisibilityBis/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChainOfResponisibilityBis {
+    class Program {
+        static void Main(string[] args) {
+            Bouncer entranceMinion = new ListMinion();
+            Bouncer eliteMinion = new EliteMinion();
+            Bouncer lastMinion = new BlackListMinion();
+            entranceMinion.SetSuccessor(eliteMinion);
+            eliteMinion.SetSuccessor(lastMinion);
+
+
+            Drunktard drunktard = new Drunktard();
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Name?");
+            drunktard.Name = Console.ReadLine();
+            Console.WriteLine("Pass?");
+            drunktard.Pass = Console.ReadLine();
+            entranceMinion.Check(drunktard);
+
+        }
+    }
+
+    class Drunktard {
+        public string Name { get; set; }
+        public string Pass { get; set; }
+    }
+
+    abstract class Bouncer {
+        protected Bouncer successor;
+
+        public void SetSuccessor(Bouncer successor) {
+            this.successor = successor;
+        }
+
+        public abstract void Check(Drunktard drunktard);
+    }
+
+    class ListMinion : Bouncer {
+
+        List<string> TheList = new List<string>();
+
+        public ListMinion() {
+            TheList.Add("Pepe");
+            TheList.Add("John");
+            TheList.Add("Jerry");
+        }
+
+        public override void Check(Drunktard drunktard) {
+            if (TheList.Contains(drunktard.Name)) {
+                this.successor.Check(drunktard);
+            }
+            else {
+                Console.WriteLine($"I'm sorry {drunktard.Name} but you are not on the list.");
+            }
+        }
+    }
+
+    class EliteMinion : Bouncer {
+
+        Dictionary<string, string> PasswordNotebook = new Dictionary<string, string>();
+
+        public EliteMinion() {
+            PasswordNotebook.Add("Pepe", "1234");
+            PasswordNotebook.Add("John", "123456");
+            PasswordNotebook.Add("Jerry", "imajerk");
+        }
+
+        public override void Check(Drunktard drunktard) {
+            if (PasswordNotebook[drunktard.Name] == drunktard.Pass) {
+                this.successor.Check(drunktard);
+            }
+            else {
+                Console.WriteLine($"Wrong password {drunktard.Name}");
+            }
+        }
+    }
+
+    class BlackListMinion : Bouncer {
+
+        List<string> BlackList = new List<string>();
+
+        public BlackListMinion() {
+            BlackList.Add("Jerry");
+        }
+
+        public override void Check(Drunktard drunktard) {
+            if (!BlackList.Contains(drunktard.Name)) {
+                this.Enter(drunktard);
+            }
+            else {
+                Console.WriteLine($"FUCK OFF {drunktard.Name}!");
+            }
+        }
+
+        public void Enter(Drunktard drunktard) {
+            Console.WriteLine($"Drunktard {drunktard.Name} has entered");
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace ChainOfResponsibility {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Scenario 1: Cooper cable");
+            Console.WriteLine("----------------------------------------");
+
+            Processor furnace1 = new Furnace();
+            Processor copperCableFactory1 = new CopperCableFactory();
+            Processor storehouse = new Storehouse();
+
+            furnace1.SetSuccessor(copperCableFactory1);
+            copperCableFactory1.SetSuccessor(storehouse);
+
+            Console.WriteLine("Input: Copper ore");
+            Material input1 = new Material("Copper ore");
+            furnace1.ProcessMaterial(input1);
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Scenario 2: Iron gear");
+            Console.WriteLine("----------------------------------------");
+
+            Processor electricFurnace1 = new ElectricFurnace();
+            Processor ironGearFactory1 = new IronGearFactory();
+
+            electricFurnace1.SetSuccessor(ironGearFactory1);
+            ironGearFactory1.SetSuccessor(storehouse);
+
+            Console.WriteLine("Input: Iron ore");
+            Material input2 = new Material("Iron ore");
+            electricFurnace1.ProcessMaterial(input2);
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Scenario 3: Refining factory");
+            Console.WriteLine("----------------------------------------");
+
+            furnace1.SetSuccessor(electricFurnace1);
+            electricFurnace1.SetSuccessor(storehouse);
+
+            Console.WriteLine("Input: Iron ore");
+            Material input3 = new Material("Iron ore");
+            furnace1.ProcessMaterial(input3);
+            Console.WriteLine("Input: Copper ore");
+            Material input4 = new Material("Copper ore");
+            furnace1.ProcessMaterial(input4);
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Scenario 4: Universal Factory");
+            Console.WriteLine("----------------------------------------");
+
+            electricFurnace1.SetSuccessor(copperCableFactory1);
+            copperCableFactory1.SetSuccessor(ironGearFactory1);
+            ironGearFactory1.SetSuccessor(storehouse);
+
+            Console.WriteLine("Input: Iron ore");
+            Material input5 = new Material("Iron ore");
+            electricFurnace1.ProcessMaterial(input5);
+            Console.WriteLine("Input: Copper ore");
+            Material input6 = new Material("Copper ore");
+            electricFurnace1.ProcessMaterial(input6);
+
+
+
+        }
+    }
+
+    class Material {
+        public string Name { get; set; }
+        public int Price { get; set; } = 0;
+
+        public Material (string name) {
+            this.Name = name;
+        }
+    }
+
+    abstract class Processor {
+        protected Processor successor;
+
+        public void SetSuccessor(Processor successor) {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessMaterial(Material material);
+    }
+
+    class Furnace : Processor {
+        public override void ProcessMaterial(Material material) {
+            if (material.Name == "Copper ore") {
+                Console.WriteLine("Furnace working");
+                material.Name = "Copper ingot";
+                material.Price += 500; 
+            }
+
+            if (successor != null) {
+                successor.ProcessMaterial(material);
+            }
+        }
+    }
+
+    class ElectricFurnace : Processor {
+        public override void ProcessMaterial(Material material) {
+            if (material.Name == "Copper ore") {
+                Console.WriteLine("ElectricFurnace working");
+                material.Name = "Copper ingot";
+                material.Price += 1000;
+            }
+            else if (material.Name == "Iron ore") {
+                Console.WriteLine("ElectricFurnace working");
+                material.Name = "Iron ingot";
+                material.Price += 1000;
+            }
+
+            if (successor != null) {
+                successor.ProcessMaterial(material);
+            }
+        }
+
+    }
+
+    class IronGearFactory : Processor {
+        public override void ProcessMaterial(Material material) {
+            if (material.Name == "Iron ingot") {
+                Console.WriteLine("IronGearFactory working");
+                material.Name = "Iron gear";
+                material.Price += 2000;
+            }
+
+            if (successor != null) {
+                successor.ProcessMaterial(material);
+            }
+        }
+    }
+
+    class CopperCableFactory : Processor {
+        public override void ProcessMaterial(Material material) {
+            if (material.Name == "Copper ingot") {
+                Console.WriteLine("CopperCableFactory working");
+                material.Name = "Copper cable";
+                material.Price += 1500;
+            }
+
+            if (successor != null) {
+                successor.ProcessMaterial(material);
+            }
+        }
+    }
+
+    class Storehouse : Processor {
+        public override void ProcessMaterial(Material material) {
+            Console.WriteLine($"New product finished: 1 unit of {material.Name} for {material.Price}$");
+        }
+    }
+
+
+}
